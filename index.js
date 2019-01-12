a56db3943cf25c6ce54b2f88289152924f000cda
xmey95 <xmey95@gmail.com>
2019-01-12 19:45:24 +0100

    Started project

---
+++ index.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({ width: 800, height: 600 })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
