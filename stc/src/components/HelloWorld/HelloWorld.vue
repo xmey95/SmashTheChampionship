7e31db904ee246dab6c158cc288b2fd33b963869
xmey95 <xmey95@gmail.com>
2019-01-13 18:59:08 +0100

    Created GUI project

---
+++ stc/src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template src="./HelloWorld.html">
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src="./HelloWorld.css">
+</style>
